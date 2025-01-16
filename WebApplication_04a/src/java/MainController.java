8f20cc2e0759cdabbaaa99884fbb453ba32b9d21
Thy0104 <baothy9a3@gmail.com>
2025-01-16 10:13:48 +0700

    Update MainController.java

--- WebApplication_04a/src/java/MainController.java
+++ WebApplication_04a/src/java/MainController.java
@@ -18,70 +18,73 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(urlPatterns = {"/MainController"})
 public class MainController extends HttpServlet {
+    public int GCD (int a, int b){
+        int min = Math.min(a, b);
+        for(int i=min;i>=1; i--){
+            if(a%i==0 && b%i==0){
+                return i;
+            }
+        }
+        return 1;
+    }
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet MainController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet MainController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        PrintWriter out = response.getWriter();
+        String txtA = request.getParameter("txtA");
+        String txtB = request.getParameter("txtB");
+        
+        if(txtA.trim().length()== 0){
+            out.println("Please enter a!:");
+            return;
         }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
+        
+        if(txtB.trim().length()== 0){
+            out.println("Please enter b!:");
+            return;
+        }
+        
+        int a = 0;
+        int b = 0;
+        try {
+            a = Integer.parseInt(txtB);
+            if (a<=0){
+                out.println("a must be greater than zero!");
+                return;
+            }
+        } catch  (Exception e) {
+            out.println("a must be integer!");
+            return;
+        }
+        
+        try {
+            b = Integer.parseInt(txtB);
+            if (b<=0){
+                out.println("b must be greater than zero!");
+                return;
+            }
+        } catch  (Exception e) {
+            out.println("b must be integer!");
+            return;
+        }
+    }    
+        
+  @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+    
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 
 }
