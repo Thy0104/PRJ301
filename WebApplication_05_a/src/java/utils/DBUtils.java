5adcab342c12762ac37e0a0a9fc5c5c1d37089ac
Thy0104 <baothy9a3@gmail.com>
2025-01-20 11:23:16 +0700

    05
    05

---
+++ WebApplication_05_a/src/java/utils/DBUtils.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author baothy2004
+ */
+public class DBUtils {
+  private static final String DB_NAME = "Web_05_a";
+  private static final String DB_USER_NAME = "sa";
+  private static final String DB_PASSWORD = "12345";
+  
